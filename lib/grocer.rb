ee64e47498499610e807204350c2d4b7fec6ea1b
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-16 01:46:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,10 @@
 def find_item_by_name_in_collection(name, collection)
-  puts "hello"
-
+  collection.each do |item|
+    if item[:item] == name
+      return item
+    end
+  end
+  return nil
 end
 
 def consolidate_cart(cart)
